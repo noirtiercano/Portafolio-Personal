d01358f70c97a5ce2170495a29bf1f59932b7689
Noirtier Cano <noirtiercano1@gmail.com>
2025-12-25 14:01:35 -0500

    feat: añadir descarga de CV en formato PDF

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -23,7 +23,7 @@ const AboutMe = () => {
 
         <div className="flex justify-center space-x-4 mt-10">
 
-          <a href="#" download="CV Noirtier Cano"
+          <a href="/Noirtier_Cano_CV_Software_Developer.pdf" download="CV_Noirtier_Cano_Dev"
             className="flex items-center space-x-2 gap2 border border-blue-600 text-blue-500 hover:bg-gradient-to-r cursor-pointer from-blue-600 to-blue-800 transition duration-300 ease-in-out hover:text-white rounded-xl text-sm p-2 font-semibold mt-5">
             
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-cv"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M11 12.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0" /><path d="M13 11l1.5 6l1.5 -6" /></svg>
