9040753a782bffe2b43d49c378fb44cb000be8cc
Noirtier Cano <noirtiercano1@gmail.com>
2025-12-25 13:29:50 -0500

    chore: actualizar año de el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="py-6 text-center text-gray-600 mt-36">
-      <p>© 2024 Noirtier Cano</p>
+      <p>2025 Noirtier Cano</p>
     </footer>
   );
 };
